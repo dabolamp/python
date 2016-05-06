daf84320037d36cf7476b8138fdf70e0db0ee407
dabolamp <david.bolamp@outlook.com>
2016-05-05 19:20:44 -0500

    Update forms.py

--- micalisoft/prueba/forms.py
+++ micalisoft/prueba/forms.py
@@ -3,6 +3,11 @@ from models import Usuarios
 
 class UsuarioForm(forms.ModelForm):
 
+   def __init__(self, *args, **kwargs):
+        super(UsuarioForm, self).__init__(*args, **kwargs)
+        self.fields['usuario'].label = "Nombre de Usuario"
+
+
    class Meta:
       model = Usuarios
       fields = '__all__'
